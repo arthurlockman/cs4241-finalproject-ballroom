b0d2920dec4913e90f36c2d5414636a61435b939
tuckerhaydon <tuckerhaydon@gmail.com>
2016-11-30 23:38:41 -0500

    Changed scraped to be a depth-first structure using promises and generators

--- scraper.js
+++ scraper.js
@@ -9,111 +9,122 @@ module.exports = {
   scrape
 };
 
+/* GLOBALS */
 const COMPETITION_YEAR_SELECTOR = '.h3'
 ,     COMPETITION_DATE_SELECTOR = 'td:nth-child(3)'
 
+ROOT_URL = 'http://www.o2cm.com/results/'
+LOG = false
+
+
+/* SCRIPT ENTRANCE */
+
 scrape()
+
+/* END SCRIPT */
 
 function scrape() {
 
-  url = 'http://www.o2cm.com/results/'
-  var competitionInfo;
-
   // Scrape the main results webpage
-  new Promise(function(fulfill, reject){
+  loadMainPage_Promise()
+  .then(forEveryCompetitionPage)
+}
+
+function loadMainPage_Promise() {
+  return new Promise(function(resolve, reject){
 
     // Perform the asynch request
-    request(url, function(error, response, html){
+    request(ROOT_URL, function(error, response, html){
+
       // Load the page
-      var $ = cheerio.load(html);
+      // var $ = cheerio.load(html);
+      var ROOT_PAGE = cheerio.load(fs.readFileSync('competitions_page.html'));
 
-      // Extract the competition info from the page
-      competitionInfo = extractAllCompetitionInfo($);
+      // TODO
+      // Assuming that all the requests finished with no errors
+      // Should check for this somehow
 
-      // When the request is done, execute the next step
-      fulfill();
+      // Return the page
+      resolve(ROOT_PAGE)
+      
     })
   })
-
-  // Scrape the webpage of every competition
-  .then(function() {
-    // Create an array of promises; one promise for every competition
-    var promiseArray = composePromiseArray(url, competitionInfo);
-
-    // Scrape all the competitions
-    return Promise.all(promiseArray);
-  })
-
-  .then(function() {
-    console.log('All webpages scraped.')
-  })
-
 }
 
-function composePromiseArray(url, competitionInfo) { 
+function forEveryCompetitionPage(ROOT_PAGE) {
+  // Extract the competition info from the page
+  var gen = competitionLinkGenerator(ROOT_PAGE);
+  while(true) {
+    var next = gen.next();
 
-  var promiseArray = []
-
-  // TODO competitionInfo.length
-  for(var i = 0; i < 1; i++) {
-    // Compose the url of a single competition
-    competitionURL = url + competitionInfo[i].ref;
-
-    // Create a promise to scrape this webpage
-    var prom = new Promise(function(resolve, reject) {
-
-      // Perform the asynch request
-      request(competitionURL, function(error, response, html){
-        // Load the page
-        var $ = cheerio.load(html);
-
-        extractSingleCompetitionInfo($);
-
-        resolve();
-
-      });
-    });
-
-    promiseArray.push(prom)
-  }
-
-  return promiseArray;
-}
-
-function extractSingleCompetitionInfo($) {
-
-  var competitors = []
-
-  $('#selEnt').each(function(i, element) {
-
-    var names = $(element).text().replace(/, /g, '#').split(/\s+/)
-
-    for(var i = 0; i < names.length; i++) {
-      var name = names[i];
-      var fullName = name.split('#');
-
-      if(fullName.length != 2) {
-        continue;
-      }
-
-      competitors.push(new Competitor(fullName[1], fullName[0]))
+    if(next.done == true){
+      break;
     }
+    else {
+      // TODO keep track of info here
+      var info = next.value
 
-  })
+      // Load the competition page
+      var url = ROOT_URL + info.ref;
 
-  return competitors;
+      loadCompetitionPage_Promise(url)
+      .then(forEveryEventPage)
+    }
+  }
 }
 
-// Extracts all competition info from the main results page:
-// http://www.o2cm.com/results/
-// Puts the info into a list of objects containing the competition name, date, year, and href
-function extractAllCompetitionInfo($) {
+function loadCompetitionPage_Promise(url) {
+  return new Promise(function(resolve, reject){
+    // Perform the asynch request
+    request(url, function(error, response, html){
+
+      // TODO PRESS THE SUBMIT BUTTON SOMEHOW
+
+      // Load the page
+      // var $ = cheerio.load(html);
+      // COMPETITION_PAGE = cheerio.load(fs.readFileSync('tufts_page.html'));
+      var COMPETITION_PAGE = cheerio.load(fs.readFileSync('tufts_page_submitted.html'));
+
+      // TODO
+      // Assuming that all the requests finished with no errors
+      // Should check for this somehow
+
+      // Return the page
+      resolve(COMPETITION_PAGE)
+      
+    })
+
+  })
+}
+
+function forEveryEventPage(COMPETITION_PAGE) {
+
+  // Extract the event info from the page
+  var gen = eventLinkGenerator(COMPETITION_PAGE);
+  while(true) {
+    var next = gen.next();
+
+    if(next.done == true){
+      break;
+    }
+    else {
+      // TODO keep track of info here
+      console.log(next.value)
+
+    }
+  }
+}
+
+
+function *competitionLinkGenerator($) {
 
   var year = '';
 
   var competitionInfo = []
 
-  $('table tr').each(function(i, element){
+  var arr = $('table tr')
+  for(var i = 0; i < arr.length; i++) {
+    var element = arr[i];
 
     // Check if it is a year
     if($(element).find(COMPETITION_YEAR_SELECTOR).text() != '') {
@@ -128,12 +139,23 @@ function extractAllCompetitionInfo($) {
 
       // Ensure no gibberish
       if(!(name == '' || date == '' || year == '' || ref == 'undefined')){
-        competitionInfo.push(new CompetitionInfo(name, date, year, ref))
+        var info = new CompetitionInfo(name, date, year, ref);
+        yield info
       }
     }
-  })
 
-  return competitionInfo
+  }
+}
+
+function *eventLinkGenerator($) {
+  var links = []
+
+  var arr = $('.h5b')
+  for(var i = 0; i < arr.length; i++) {
+    var element = arr[i];
+
+    console.log($(element).find('a').text().trim())
+  }
 }
 
 class Competitor {
