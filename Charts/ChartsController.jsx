2dfa1bc2ea22132e58361a8e9f4cf8b1e276b6a2
tuckerhaydon <tuckerhaydon@gmail.com>
2016-12-14 23:32:36 -0500

    Updated histograms

--- Charts/ChartsController.jsx
+++ Charts/ChartsController.jsx
@@ -8,7 +8,8 @@ const COLORS = [
     '#F2317A',
     '#FF9824',
     '#58CF6C',
-    '#000000'
+    '#000000',
+    ''
 ]
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -18,38 +19,71 @@ function getRandomInt(min, max) {
 class ChartsController extends React.Component {
     constructor(props) {
         super(props);
+        // console.log(props.data)
+
+        var numJudges = Object.keys(props.data.competitors[0].marks).length
+        var lbl = []
+        for(var i = 0; i < numJudges; i++) {
+            lbl.push(i+" marks")
+        }
+
+        var totalMarksArray = props.data.competitors.map(function(competitor) {
+            return competitor.totalMarks
+        })
+
+        totalMarksArray.sort()
+
+        var markFrequencyDict = {};
+
+        // All mark frequecy start at 0
+        for(var i = 0; i < numJudges; i++) {
+            markFrequencyDict[i] = 0
+        }
+
+        // For every markTotal, increment the dictionary value
+        for(var i = 0; i< totalMarksArray.length; i++) {
+            var num = totalMarksArray[i];
+            markFrequencyDict[num] = markFrequencyDict[num]+1
+        }
+
+        var markFrequencyArr = []
+        for(var i = 0; i < numJudges; i++) {
+            markFrequencyArr.push(markFrequencyDict[i])
+        }
+
+        console.log(markFrequencyArr)
+
         this.state = this.initialState = {
-            data: [],
+            data: [markFrequencyArr],
             series: [
-                'Am. Waltz', 'Am. Tango'
+                props.data.danceName
             ],
-            labels: [
-                '0 marks', '1 mark', '2 marks', '3 marks', '4 marks'
-            ],
-            colors: ['#43A19E', '#7B43A1', '#F2317A', '#FF9824', '#58CF6C']
+            labels: lbl,
+            colors: COLORS
         };
     }
 
     componentDidMount() {
-        this.populateArray();
+        // this.populateArray();
     }
-    populateArray() {
-        var data = [],
-            series = 2, // Number of charts
-            serieLength = 5;
 
-        for (var i = series; i--;) {
-            var tmp = [];
+    // populateArray() {
+    //     var data = [],
+    //         series = 1, // Number of charts
+    //         serieLength = 5;
 
-            for (var j = serieLength; j--;) {
-                tmp.push(getRandomInt(0, 20));
-            }
+    //     for (var i = series; i--;) {
+    //         var tmp = [];
 
-            data.push(tmp);
-        }
+    //         for (var j = serieLength; j--;) {
+    //             tmp.push(getRandomInt(0, 20));
+    //         }
 
-        this.setState({data: data});
-    }
+    //         data.push(tmp);
+    //     }
+
+    //     this.setState({data: data});
+    // }
     
     render() {
         return (
